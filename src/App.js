8c46bc4ce5cdca5e84902bc8ac569213da4093d6
nikunjdudhat <nikunjdudhat2002@gmail.com>
2022-07-12 08:20:29 +0530

    add react persist

--- src/App.js
+++ src/App.js
@@ -9,21 +9,25 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import { countorStore } from './Redux/Store';
 import Counter from './container/Counter/Counter';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
 
-  const store = countorStore()
+  const {store, persistor} = countorStore()
+  
 
   return (
     <Provider store={store}>
-      <Layout>
-        <Switch>
-            <Route exact path={"/"} component={Medicine} />
-            <Route exact path={"/Medicine"} component={Medicine} />
-            <Route exact path={"/Doctor"} component={Doctor} />
-            <Route exact path={"/Counter"} component={Counter} />
-        </Switch>
-      </Layout>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Switch>
+              <Route exact path={"/"} component={Medicine} />
+              <Route exact path={"/Medicine"} component={Medicine} />
+              <Route exact path={"/Doctor"} component={Doctor} />
+              <Route exact path={"/Counter"} component={Counter} />
+          </Switch>
+        </Layout>
+      </PersistGate>
     </Provider>
   );
 }
